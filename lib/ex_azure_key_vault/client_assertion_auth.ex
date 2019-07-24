d16146a918a9d4241ede4a4eed28cccb8a69e303
Ramon da Silva <radasi@microsoft.com>
2019-07-24 00:45:09 -0300

    Reuse auth_url method

--- lib/ex_azure_key_vault/client_assertion_auth.ex
+++ lib/ex_azure_key_vault/client_assertion_auth.ex
@@ -107,7 +107,7 @@ defmodule ExAzureKeyVault.ClientAssertionAuth do
     jti = Joken.generate_jti()
     nbf = Joken.current_time()
     exp = Joken.current_time() + 60 # in 1 minute
-    aud = "https://login.microsoftonline.com/#{tenant_id}/oauth2/v2.0/token"
+    aud = auth_url(tenant_id)
     {:ok, claims} = Joken.generate_claims(%{}, %{sub: sub, iss: iss, jti: jti, nbf: nbf, exp: exp, aud: aud})
     {:ok, jwt, _} = Joken.encode_and_sign(claims, signer)
     jwt
