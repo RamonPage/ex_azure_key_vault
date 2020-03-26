952bbd10384d8b612d345ae4f3b504e10881a592
Ramon da Silva <radasi@microsoft.com>
2020-03-26 00:19:49 -0300

    Refactor version task

--- build/mix/tasks/version.ex
+++ build/mix/tasks/version.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.Version do
   def run(_) do
     Mix.shell().info("Getting latest release...")
     {:ok, latest} = File.read("LATEST.md")
-    version = Regex.run(~r/## v(.*)\n\n/, latest) |> Enum.at(1)
+    version = (Regex.run(~r/## v(.*)\n\n/, latest) || []) |> Enum.at(1)
     latest_release = Regex.replace(~r/# Latest release\n/, latest, "")
 
     unless version do
@@ -48,13 +48,16 @@ defmodule Mix.Tasks.Version do
     File.write("mix.exs", Regex.replace(~r/@version "(.*)"/, mix_exs, "@version \"#{version}\""))
     Mix.shell().info("➜ mix.exs updated!")
 
-    if latest_release do
-      Mix.shell().info("Updating CHANGELOG...")
-      {:ok, changelog} = File.read("CHANGELOG.md")
+    Mix.shell().info("Updating CHANGELOG...")
+    {:ok, changelog} = File.read("CHANGELOG.md")
+
+    if !Regex.run(~r/## v#{version}\n\n/, changelog) do
       changelog = Regex.replace(~r/# Changelog\n/, changelog, "")
       updated_changelog = "# Changelog\n#{latest_release}\n#{changelog}"
       File.write("CHANGELOG.md", updated_changelog)
       Mix.shell().info("➜ CHANGELOG updated!")
+    else
+      Mix.shell().info("➜ CHANGELOG is already up to date.")
     end
   end
 end
