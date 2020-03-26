59e645afd22f82b978000c182b18b24611284f82
Ramon da Silva <radasi@microsoft.com>
2020-03-25 21:43:39 -0300

    Update version using LATEST file

--- lib/mix/tasks/version.ex
+++ lib/mix/tasks/version.ex
@@ -2,9 +2,18 @@ defmodule Mix.Tasks.Version do
   use Mix.Task
 
   @impl Mix.Task
-  def run(args) do
-    version = Enum.at(args, 0)
-    description = Enum.at(args, 1)
+  def run(_) do
+    Mix.shell().info("Getting latest release...")
+    {:ok, latest} = File.read("LATEST.md")
+    version = Regex.run(~r/## v(.*)\n\n/, latest) |> Enum.at(1)
+    latest_release = Regex.replace(~r/# Latest release\n/, latest, "")
+
+    unless version do
+      Mix.shell().info("Version not found.")
+      exit(:normal)
+    end
+
+    Mix.shell().info("➜ Version #{version} found.")
 
     Mix.shell().info("Updating DOCS...")
     {:ok, docs} = File.read("DOCS.md")
@@ -18,9 +27,7 @@ defmodule Mix.Tasks.Version do
       )
     )
 
-    Mix.shell().info("DOCS updated!")
-
-    Mix.shell().info("")
+    Mix.shell().info("➜ DOCS updated!")
 
     Mix.shell().info("Updating README...")
     {:ok, readme} = File.read("README.md")
@@ -34,25 +41,20 @@ defmodule Mix.Tasks.Version do
       )
     )
 
-    Mix.shell().info("README updated!")
-
-    Mix.shell().info("")
+    Mix.shell().info("➜ README updated!")
 
     Mix.shell().info("Updating mix.exs...")
     {:ok, mix_exs} = File.read("mix.exs")
     File.write("mix.exs", Regex.replace(~r/@version "(.*)"/, mix_exs, "@version \"#{version}\""))
-    Mix.shell().info("mix.exs updated!")
+    Mix.shell().info("➜ mix.exs updated!")
 
-    Mix.shell().info("")
-
-    if description do
+    if latest_release do
       Mix.shell().info("Updating CHANGELOG...")
       {:ok, changelog} = File.read("CHANGELOG.md")
       changelog = Regex.replace(~r/# Changelog\n/, changelog, "")
-      description = Regex.replace(~r/\\n/, description, "\n")
-      updated_changelog = "# Changelog\n\n## v#{version}\n\n#{description}\n#{changelog}"
+      updated_changelog = "# Changelog\n#{latest_release}\n#{changelog}"
       File.write("CHANGELOG.md", updated_changelog)
-      Mix.shell().info("CHANGELOG updated!")
+      Mix.shell().info("➜ CHANGELOG updated!")
     end
   end
 end
